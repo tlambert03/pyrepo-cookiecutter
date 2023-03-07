7073d2c8818d5998d103edb3379c89e0d1c48f3a
Talley Lambert <talley.lambert@gmail.com>
2023-03-07 14:59:42 -0500

    simple  version

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -1,10 +1,5 @@
 """{{ cookiecutter.project_short_description }}"""
-from importlib.metadata import PackageNotFoundError, version
 
-try:
-    __version__ = version("{{cookiecutter.project_name}}")
-except PackageNotFoundError:
-    __version__ = "uninstalled"
-
+__version__ = "0.0.1"
 __author__ = "{{ cookiecutter.full_name }}"
 __email__ = "{{ cookiecutter.email }}"
